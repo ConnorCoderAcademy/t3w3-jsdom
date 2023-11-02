2a1ac499b93e2581d3e49abf0b44a97c9e1c3cca
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-11-02 19:24:00 +1100

    added functions/buttons

---
+++ themeControl.js
@@ -0,0 +1,30 @@
+const themes = {
+    light: {
+        background: "white",
+        text: "grey"
+    },
+    dark: {
+        background: "grey",
+        text: "white"
+    }
+}
+
+function changeCSSTheme (themeName) {
+    // console.log(themes[themeName]["background"]);
+    // document.documentElement.style.setProperty("--background", themes[themeName]["background"]);
+    // document.documentElement.style.setProperty("--text", themes[themeName]["text"]);
+    for (const variable in themes[themeName]) {
+        document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+    }
+}
+
+function toggleLightDark() {
+    let currentBackgroundColour = getComputedStyle(document.documentElement).getPropertyValue('--background');
+    console.log(currentBackgroundColour);
+    if (currentBackgroundColour == "white") {
+        changeCSSTheme("dark");
+    } else {
+        changeCSSTheme("light");
+    }
+}
+
